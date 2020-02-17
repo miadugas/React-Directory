38c69f214b7ea0ef91c7fb8d74ecc607ad7e2050
Mia Dugas <miadugas@outlook.com>
2020-02-17 00:10:13 -0700

    Updates

---
+++ precache-manifest.89f71f50fa12e0cd25f12dcfeadadc6c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2fe191db8ed6950f092763a96f44e1",
+    "url": "/React-Directory/index.html"
+  },
+  {
+    "revision": "6f892538b2b67ca4c42d",
+    "url": "/React-Directory/static/css/2.ca8c5883.chunk.css"
+  },
+  {
+    "revision": "8e98cbe0844242118c28",
+    "url": "/React-Directory/static/css/main.f23f0195.chunk.css"
+  },
+  {
+    "revision": "6f892538b2b67ca4c42d",
+    "url": "/React-Directory/static/js/2.90366f94.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/React-Directory/static/js/2.90366f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e98cbe0844242118c28",
+    "url": "/React-Directory/static/js/main.0b896e10.chunk.js"
+  },
+  {
+    "revision": "d36c958b7ca4871836e4",
+    "url": "/React-Directory/static/js/runtime-main.dec28db8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Directory/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Directory/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Directory/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Directory/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Directory/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
